2efa0b666de60945cdc7f12a87108954a2fcfc66
Daman Arora <daman397@hotmail.com>
2023-01-18 11:14:51 +0000

    Created an initial react page to work off of

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return (
+        <header className="App-header">
+            <h1>
+                Welcome to OAXACA
+            </h1>
+            <div>
+                <p>"This is the restaurant app concept"</p>
+            </div>
+        </header>
+    );
+}
+
+export default App;
