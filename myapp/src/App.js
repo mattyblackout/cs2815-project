9722714ae65e186a3680d615e867b65eefe7fb48
Daman Arora <daman397@hotmail.com>
2023-01-18 13:06:19 +0000

    Added collapsible buttons to the menu

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,16 +1,54 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    return (
-        <header className="App-header">
-            <h1>
-                Welcome to OAXACA
-            </h1>
-            <div>
-                <p>"This is the restaurant app concept"</p>
-            </div>
-        </header>
-    );
+  const [mainOpen, setMainOpen] = useState(false);
+  const [sidesOpen, setSidesOpen] = useState(false);
+  const [dessertsOpen, setDessertsOpen] = useState(false);
+  const [drinksOpen, setDrinksOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Oaxaca Restaurant</h1>
+      </header>
+      <div className="menu-container">
+        <h2>Menu</h2>
+        <button onClick={() => setMainOpen(!mainOpen)}>Main Dishes</button>
+        {mainOpen && (
+          <ul>
+            <li>Taco al Pastor - $5.99</li>
+            <li>Mole Poblano - $12.99</li>
+            <li>Tamales Oaxaqueños - $8.99</li>
+            <li>Enchiladas de Mole - $10.99</li>
+          </ul>
+        )}
+        <button onClick={() => setSidesOpen(!sidesOpen)}>Sides</button>
+        {sidesOpen && (
+          <ul>
+            <li>Refried Beans - $2.99</li>
+            <li>Rice - $2.99</li>
+            <li>Guacamole - $4.99</li>
+          </ul>
+        )}
+        <button onClick={() => setDessertsOpen(!dessertsOpen)}>Desserts</button>
+        {dessertsOpen && (
+          <ul>
+            <li>Churros con Chocolate - $6.99</li>
+            <li>Flan - $5.99</li>
+          </ul>
+        )}
+        <button onClick={() => setDrinksOpen(!drinksOpen)}>Drinks</button>
+        {drinksOpen && (
+          <ul>
+            <li>Horchata - $2.99</li>
+            <li>Agua Fresca - $2.99</li>
+            <li>Margarita - $7.99</li>
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
