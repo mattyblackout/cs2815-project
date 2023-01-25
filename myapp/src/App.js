4501368cd70e5194a213f890193f989cb4c7f9a6
Daman Arora <daman397@hotmail.com>
2023-01-25 12:53:44 +0000

    Added a prompt to take the clients table number before they look at the menu

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,100 +1,97 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './logo.png';
-import './fonts/Bayon-Regular.ttf'
-import splash from './splash-image.jpg'
+import './fonts/Bayon-Regular.ttf';
+import splash from './splash-image.jpg';
 
 function App() {
-    const [mainOpen, setMainOpen] = useState(false);
-    const [sidesOpen, setSidesOpen] = useState(false);
-    const [dessertsOpen, setDessertsOpen] = useState(false);
-    const [drinksOpen, setDrinksOpen] = useState(false);
+    const [expanded, setExpanded] = useState("");
+    const [tableNumber, setTableNumber] = useState("");
+    const [showPrompt, setShowPrompt] = useState(true);
 
-    const handleMainClick = () => {
-        setMainOpen(!mainOpen);
-        setSidesOpen(false);
-        setDessertsOpen(false);
-        setDrinksOpen(false);
+    const handleButtonClick = (item) => {
+        setExpanded(item);
     }
 
-    const handleSidesClick = () => {
-        setMainOpen(false);
-        setSidesOpen(!sidesOpen);
-        setDessertsOpen(false);
-        setDrinksOpen(false);
-    }
+    useEffect(() => {
+        if (showPrompt) {
+            setTableNumber(window.prompt('What is your table number?'));
+            setShowPrompt(false);
+        }
+    }, [showPrompt]);
+  return (
 
-    const handleDessertsClick = () => {
-        setMainOpen(false);
-        setSidesOpen(false);
-        setDessertsOpen(!dessertsOpen);
-        setDrinksOpen(false);
-    }
+    <div className="App">
 
-    const handleDrinksClick = () => {
-        setMainOpen(false);
-        setSidesOpen(false);
-        setDessertsOpen(false);
-        setDrinksOpen(!drinksOpen);
-    }
+      <header className="App-header">
+        <img src={logo} alt="the logo" className="header-image" />
+      </header>
+      <div className="splash-div">
+        <img src={splash} alt="splash" className={"splash-image"} />
+      </div>
+        <div className="order-container">
+            <p> Table {tableNumber}</p>
+        </div>
+      <div className="menu-container">
+        <div className="separate" />
 
-    return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} alt="the logo" className="header-image"/>
-                </header>
-                <div className="splash-div">
-                    <img src={splash} alt="splash" className={"splash-image"}/>
-                    </div>
-
-                <div className="menu-container">
-                    <h2> </h2>
-                    <div className="menu-categories-container">
-                        <div className="menu-category-container">
-                            <button onClick={handleMainClick}>Mains</button>
-                            {mainOpen && (
-                                    <ul>
-                                        <li>Taco al Pastor - $5.99</li>
-                                        <li>Mole Poblano - $12.99</li>
-                                        <li>Tamales Oaxaqueños - $8.99</li>
-                                        <li>Enchiladas de Mole - $10.99</li>
-                                    </ul>
-                                    )}
-                        </div>
-                        <div className="menu-category-container">
-                            <button onClick={handleSidesClick}>Sides</button>
-                            {sidesOpen && (
-                                    <ul>
-                                        <li>Refried Beans - $2.99</li>
-                                        <li>Rice - $2.99</li>
-                                        <li>Guacamole - $4.99</li>
-                                    </ul>
-                                    )}
-                        </div>
-                        <div className="menu-category-container">
-                            <button onClick={handleDessertsClick}>Desserts</button>
-                            {dessertsOpen && (
-                                    <ul>
-                                        <li>Churros con Chocolate - $6.99</li>
-                                        <li>Flan - $5.99</li>
-                                    </ul>
-                                    )}
-                        </div>
-                        <div className="menu-category-container">
-                            <button onClick={handleDrinksClick}>Drinks</button>
-                            {drinksOpen && (
-                                    <ul>
-                                        <li>Horchata - $2.99</li>
-                                        <li>Agua Fresca - $2.99</li>
-                                        <li>Margarita - $7.99</li>
-                                    </ul>
-                                    )}
-                        </div>
-                    </div>
-                </div>
+        <div className="menu-categories-container">
+            <div className="menu-category-container">
+                <button onClick={() => handleButtonClick("mains")}>Mains</button>
             </div>
-            );
+            <div className="menu-category-container">
+                <button onClick={() => handleButtonClick("sides")}>Sides</button>
+            </div>
+            <div className="menu-category-container">
+                <button onClick={() => handleButtonClick("desserts")}>Desserts</button>
+            </div>
+            <div className="menu-category-container">
+                <button onClick={() => handleButtonClick("drinks")}>Drinks</button>
+            </div>
+        </div>
+      </div>
+        <div className="separate">
+        </div>
+        {expanded === "mains" && (
+                <div className='expanded-div'>
+                    <ul>
+                        <li>Taco al Pastor - $5.99</li>
+                        <li>Mole Poblano - $12.99</li>
+                        <li>Tamales Oaxaquenos - $8.99</li>
+                        <li>Enchiladas de Mole - $10.99</li>
+                    </ul>
+                </div>
+                )}
+        {expanded === "sides" && (
+                <div className='expanded-div'>
+                    <ul>
+                        <li>Refried Beans - $2.99</li>
+                        <li>Rice - $2.99</li>
+                        <li>Guacamole - $4.99</li>
+                    </ul>
+                </div>
+                )}
+        {expanded === "desserts" && (
+                <div className='expanded-div'>
+                    <ul>
+                        <li>Churros con Chocolate - $6.99</li>
+                        <li>Flan - $5.99</li>
+                    </ul>
+                </div>
+                )}
+        {expanded === "drinks" && (
+                <div className='expanded-div'>
+                    <ul>
+                        <li>Horchata - $2.99</li>
+                        <li>Agua Fresca - $2.99</li>
+                        <li>Margarita - $7.99</li>
+                    </ul>
+                </div>
+                )}
+    </div>
+    );
 }
+
 export default App;
 
 
