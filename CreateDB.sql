1e8335d710d30d7c38282e6d97c8c1dbd7f574d6
Arora Daman (2021) ZKAC410 <daman.arora.2021@live.rhul.ac.uk>
2023-02-02 17:19:20 +0000

    Delete CreateDB.sql

--- CreateDB.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE menu (
-    id SERIAL,
-    name varchar(255),
-    ingredients varchar(255),
-    calories int,
-    category varchar(255),
-    available boolean,
-    primary key (id)
-    );
