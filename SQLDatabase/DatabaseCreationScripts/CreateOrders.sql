d99fb8e510314f3421ea406ea0884388abf57d07
ZJAC028 <zjac028@live.rhul.ac.uk>
2023-02-14 15:58:39 +0000

    Added Kitchen Orders tables & sample data Added the SQL scripts and Sample Data for the Orders and Order_Items table that is used for the Kitchen Orders page.

---
+++ SQLDatabase/DatabaseCreationScripts/CreateOrders.sql
@@ -0,0 +1,4 @@
+CREATE TABLE orders (
+    order_number SERIAL PRIMARY KEY,
+    time_ordered TIME
+);
---
+++ SQLDatabase/SampleData/Order_ItemsData.sql
@@ -0,0 +1,2 @@
+INSERT INTO order_items (order_number, item_id, item_quantity)
+VALUES (1, 1, 2), (2, 2, 1), (3, 3, 3), (4, 4, 2), (5, 5, 1), (2, 7, 2), (4, 11, 3)
