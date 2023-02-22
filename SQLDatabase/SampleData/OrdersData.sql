1d66fe18de7c1cedbca4841636bd31ef5eeb1aa1
ZJAC028 <zjac028@live.rhul.ac.uk>
2023-02-22 17:05:29 +0000

    Added new column 'completed' to Orders table I have added a new column called completed of type boolean, so that once the kitchen staff have finished the order and the waiters have taken it, the column will be cahnged to TRUE, and the order will no longer be displayed on the Kitchen Orders page.

--- SQLDatabase/SampleData/OrdersData.sql
+++ SQLDatabase/SampleData/OrdersData.sql
@@ -1,2 +1,2 @@
 INSERT INTO orders (time_ordered, confirmed)
-VALUES ('10:00:00', TRUE), ('11:00:00', TRUE), ('12:00:00', TRUE), ('13:00:00', TRUE), ('14:00:00', TRUE), ('15:00:00', FALSE)
+VALUES ('10:00:00', TRUE, TRUE), ('11:00:00', TRUE, TRUE), ('12:00:00', TRUE, TRUE), ('13:00:00', TRUE, TRUE), ('14:00:00', TRUE, TRUE), ('15:00:00', FALSE, TRUE)
