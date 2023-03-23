e3d23a93298df85776f6ca6f8c42d28a971496f4
Daman Arora <daman397@hotmail.com>
2023-03-23 20:00:51 +0000

    Created buttons to filter the food for vegan, vegetarian and dairy-free

--- myapp/src/pages/Menu.js
+++ myapp/src/pages/Menu.js
@@ -3,7 +3,7 @@ import '../css/Menu.css';
 import logo from '../logo.png';
 import '../fonts/Bayon-Regular.ttf';
 import splash from '../splash-image.jpg';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 let show = true;
 
@@ -24,14 +24,14 @@ function Menu() {
      */
     const infoPopup = (id) => {
         fetch(`http://localhost:3000/menu/info/${id}`)
-          .then((response) => response.json())
-          .then((data) => {
-            alert(`------------------ Ingredients / Allergens ------------------\n\n${data[0].allingredients}`)
-          })
-          .catch((error) => {
-            console.log("Error: ", error);
-          });
-      };
+            .then((response) => response.json())
+            .then((data) => {
+                alert(`------------------ Ingredients / Allergens ------------------\n\n${data[0].allingredients}`)
+            })
+            .catch((error) => {
+                console.log("Error: ", error);
+            });
+    };
 
     /**
      * Adds an item, specified by item id, to an order.
@@ -136,16 +136,14 @@ function Menu() {
     };
 
     // Responsible for sending a help request to the server
-    const sendHelpRequest = (event) => {
+    const sendHelpRequest = () => {
         window.alert("Help Requested!");
         // Send data to server to insert into database
         fetch(`http://localhost:3000/helpRequest/${tableNumber}`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({tableNumber}),
+            method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({tableNumber}),
         })
     }
-    
+
 
     // Prompts user to enter their table number
     useEffect(() => {
@@ -156,144 +154,155 @@ function Menu() {
     }, []);
 
     // Main front-end code for menu page
-    return (
-        <div className="App">
-            <header className="App-header">
-                <Link to="/">
-                    <img src={logo} alt="the logo" className="header-image"/>
+    return (<div className="App">
+        <header className="App-header">
+            <Link to="/">
+                <img src={logo} alt="the logo" className="header-image"/>
+            </Link>
+            <div>
+                <Link to="/login">
+                    <button className="login-button">Login</button>
                 </Link>
-                <div>
-                    <Link to="/login">
-                        <button className="login-button">Login</button>
-                    </Link>
-                    <Link to="/cart">
-                        <button className="cart-button">Cart</button>
-                    </Link>
-                </div>
-            </header>
-            <div className="splash-image">
-                <img src={splash} alt="splash" className={"splash-image"}/>
+                <Link to="/cart">
+                    <button className="cart-button">Cart</button>
+                </Link>
             </div>
-            <div className="menu-container">
-                <div className="menu-categories-container">
-                    <div className="menu-category-container">
-                        <button onClick={() => handleButtonClick("mains")}>Mains</button>
+        </header>
+        <div className="splash-image">
+            <img src={splash} alt="splash" className={"splash-image"}/>
+        </div>
+        <div className="menu-container">
+            <div className="menu-categories-container">
+                <div className="menu-category-container">
+                    <button onClick={() => handleButtonClick("mains")}>Mains</button>
+                </div>
+                <div className="menu-category-container">
+                    <button onClick={() => handleButtonClick("sides")}>Sides</button>
+                </div>
+                <div className="menu-category-container">
+                    <button onClick={() => handleButtonClick("desserts")}>Desserts</button>
+                </div>
+                <div className="menu-category-container">
+                    <button onClick={() => handleButtonClick("drinks")}>Drinks</button>
+                </div>
+
+                <div className="ordering-container">
+                    <h1 className="table">TABLE</h1>
+                    <p className="table-number">{tableNumber}</p>
+                    <hr className="underline"></hr>
+                    <h1 className="simple-text">YOUR ORDER</h1>
+                    <h4 className={"GAP"}/>
+                    {order.map((item, index) => (<div className="order-row">
+                        <div key={index}>
+                            <div className="item-name">{item.quantity} {item.name}</div>
+                            <button className="remove" onClick={() => handleRemove(item)}>-</button>
+                            <div className="money">£{(item.price * item.quantity).toFixed(2)}</div>
+                        </div>
+                    </div>))}
+                    <hr className="underline"></hr>
+                    <h1 className="simple-text">TOTAL</h1>
+                    <h2 className="money">£{totalMoney.toFixed(2)}</h2>
+                    <br/>
+                    <div>
+                        <button className="invisible-button"></button>
+                        <Link to="/cart">
+                            <button className="checkout" onClick={() => handleCheckout()}>CHECKOUT</button>
+                        </Link>
+                        <button className="help-button" onClick={() => sendHelpRequest()}>Call Waiter</button>
                     </div>
-                    <div className="menu-category-container">
-                        <button onClick={() => handleButtonClick("sides")}>Sides</button>
-                    </div>
-                    <div className="menu-category-container">
-                        <button onClick={() => handleButtonClick("desserts")}>Desserts</button>
-                    </div>
-                    <div className="menu-category-container">
-                        <button onClick={() => handleButtonClick("drinks")}>Drinks</button>
-                    </div>
-                    <div className="ordering-container">
-                        <h1 className="table">TABLE</h1>
-                        <p className="table-number">{tableNumber}</p>
-                        <hr className="underline"></hr>
-                        <h1 className="simple-text">YOUR ORDER</h1>
-                        <h4 className={"GAP"}/>
-                        {order.map((item, index) => (
-                                <div className= "order-row">
-                                    <div key={index}>
-                                        <div className="item-name">{item.quantity} {item.name}</div>
-                                        <button className="remove" onClick={() => handleRemove(item)}>-</button>
-                                        <div className="money">£{(item.price * item.quantity).toFixed(2)}</div>
-                                    </div>
-                                </div>
-                                ))}
-                        <hr className="underline"></hr>
-                        <h1 className="simple-text">TOTAL</h1>
-                        <h2 className="money">£{totalMoney.toFixed(2)}</h2>
+                </div>
+            </div>
+        </div>
+        <div className="separate"/>
+        {expanded === "mains" && (<div className='expanded-div'>
+            <div className="filters">
+                <button className="vegan-filter">Vegan</button>
+                <button className="vegetarian-filter">Vegetarian</button>
+                <button className="dairy-filter">Dairy Free</button>
+            </div>
+            <div className='menu-items-container'>
+                {mains.map((item) => (<>
+                    <div className='food-item-container'>
+                        <div className='menu-items' key={item.id}>{item.name} -
+                            £{item.price}&nbsp;&nbsp;&nbsp;
+                            <div className='calories' key={item.id}>({item.calories} KCAL)</div>
+                            <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
+                            <br/></div>
+                        <div className='description' key={item.id}>{item.description} <br/></div>
+                        <button className='add-button' onClick={() => addToOrder(item)}> Add To Order
+                        </button>
                         <br/>
                         <div>
-                            <button className="invisible-button"></button>
-                            <Link to = "/cart">
-                                <button className="checkout" onClick={handleCheckout}>CHECKOUT</button>
-                            </Link>
-                            <button class="help-button" onClick={sendHelpRequest}>Call Waiter</button>
+                            <hr className="solid"></hr>
                         </div>
                     </div>
-                </div>
+                </>))}
             </div>
-            <div className="separate"/>
-            {expanded === "mains" && (
-                <div className='expanded-div'>
-                    <div className='menu-items-container'>
-                        {mains.map((item) => (
-                            <>
-                                <div className='food-item-container'>
-                                    <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
-                                        <div className='calories' key={item.id}>({item.calories} KCAL)</div>
-                                        <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
-                                        <br/></div>
-                                    <div className='description' key={item.id}>{item.description} <br/></div>
-                                    <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
-                                    <br/>
-                                    <div><hr class="solid"></hr></div>
-                                </div>
-                            </>
-                        ))}
+        </div>)}
+        {expanded === "sides" && (<div className='expanded-div'>
+            <div className="filters">
+                <button className="vegan-filter">Vegan</button>
+                <button className="vegetarian-filter">Vegetarian</button>
+                <button className="dairy-filter">Dairy Free</button>
+            </div>
+            <div className='menu-items-container'>
+                {sides.map((item) => (<>
+                    <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
+                        <div className='calories' key={item.id}>({item.calories} KCAL)</div>
+                        <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
+                        <br/></div>
+                    <div className='description' key={item.id}>{item.description} <br/></div>
+                    <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
+                    <br/>
+                    <div>
+                        <hr className="solid"></hr>
                     </div>
-                </div>
-            )}
-            {expanded === "sides" && (
-                <div className='expanded-div'>
-                    <div className='menu-items-container'>
-                        {sides.map((item) => (
-                            <>
-                                <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
-                                    <div className='calories' key={item.id}>({item.calories} KCAL)</div>
-                                    <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
-                                    <br/></div>
-                                <div className='description' key={item.id}>{item.description} <br/></div>
-                                <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
-                                <br/>
-                                <div><hr class="solid"></hr></div>
-                            </>
-                        ))}
+                </>))}
+            </div>
+        </div>)}
+        {expanded === "desserts" && (<div className='expanded-div'>
+            <div className="filters">
+                <button className="vegan-filter">Vegan</button>
+                <button className="vegetarian-filter">Vegetarian</button>
+                <button className="dairy-filter">Dairy Free</button>
+            </div>
+            <div className='menu-items-container'>
+                {desserts.map((item) => (<>
+                    <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
+                        <div className='calories' key={item.id}>({item.calories} KCAL)</div>
+                        <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
+                        <br/></div>
+                    <div className='description' key={item.id}>{item.description}<br/></div>
+                    <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
+                    <br/>
+                    <div>
+                        <hr className="solid"></hr>
                     </div>
-                </div>
-            )}
-            {expanded === "desserts" && (
-                <div className='expanded-div'>
-                    <div className='menu-items-container'>
-                        {desserts.map((item) => (
-                            <>
-                                <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
-                                    <div className='calories' key={item.id}>({item.calories} KCAL)</div>
-                                    <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
-                                    <br/></div>
-                                <div className='description' key={item.id}>{item.description}<br/></div>
-                                <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
-                                <br/>
-                                <div><hr class="solid"></hr></div>
-                            </>
-                        ))}
+                </>))}
+            </div>
+        </div>)}
+        {expanded === "drinks" && (<div className='expanded-div'>
+            <div className="filters">
+                <button className="vegan-filter">Vegan</button>
+                <button className="vegetarian-filter">Vegetarian</button>
+                <button className="dairy-filter">Dairy Free</button>
+            </div>
+            <div className='menu-items-container'>
+                {drinks.map((item) => (<>
+                    <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
+                        <div className='calories' key={item.id}>({item.calories} KCAL)</div>
+                        <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
+                        <br/></div>
+                    <div className='description' key={item.id}>{item.description} <br/></div>
+                    <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
+                    <br/>
+                    <div>
+                        <hr className="solid"></hr>
                     </div>
-                </div>
-            )}
-            {expanded === "drinks" && (
-                <div className='expanded-div'>
-                    <div className='menu-items-container'>
-                        {drinks.map((item) => (
-                            <>
-                                <div className='menu-items' key={item.id}>{item.name} - £{item.price}&nbsp;&nbsp;&nbsp;
-                                    <div className='calories' key={item.id}>({item.calories} KCAL)</div>
-                                    <button className='information' onClick={() => infoPopup(item.id)}>ⓘ</button>
-                                    <br/></div>
-                                <div className='description' key={item.id}>{item.description} <br/></div>
-                                <button className='add-button' onClick={() => addToOrder(item)}> Add To Order</button>
-                                <br/>
-                                <div><hr class="solid"></hr></div>
-                            </>
-                        ))}
-                    </div>
-                </div>
-            )}
-        </div>
-    )
+                </>))}
+            </div>
+        </div>)}
+    </div>)
 }
 
 export default Menu;
